e583b87a269070c9dccc1f231e5370382dfd7d71
kenny-chua <kenny.chua.edu@outlook.com>
2025-01-21 13:09:56 -0500

    Initial Attempt

---
+++ hangman.py
@@ -0,0 +1,22 @@
+import random
+
+class Hangman:
+
+    secret_movie = random.choice(list(movies.txt))
+
+    def __init__(self, max_guesses = 10):
+        """Initialize"""
+        self.number_of_guesses = number_of_guesses
+        self.current_guessed_letter = current_guessed_letter
+
+    def guess_a_letter():
+        pass
+
+    def update_hangman():
+        pass
+
+    def win_lose():
+        pass
+
+    def game_status():
+        pass
